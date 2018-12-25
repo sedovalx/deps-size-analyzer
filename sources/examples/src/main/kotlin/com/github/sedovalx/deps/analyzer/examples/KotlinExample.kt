426da4b83360953763cbcb40e90e471210a7316f
Alexander Sedov <alexander.sedov@jetbrains.com>
2018-12-25 15:33:40 +0300

    Updated example run script

--- sources/examples/src/main/kotlin/com/github/sedovalx/deps/analyzer/examples/KotlinExample.kt
+++ sources/examples/src/main/kotlin/com/github/sedovalx/deps/analyzer/examples/KotlinExample.kt
@@ -7,7 +7,7 @@ fun main() {
     val client = DepsClient()
 
     listOf(
-        "com.squareup.okhttp3:okhttp:3.11.0",
+        "com.squareup.okhttp3:okhttp:3.12.0",
         "org.glassfish.jersey.core:jersey-client:2.27",
         "com.google.http-client:google-http-client:1.25.0"
     ).forEach {
